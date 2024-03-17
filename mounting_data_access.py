8334837e6d6bd90c638324b9cfa6b0527cdf5a1f
MarceloBritoMedeiros <MarceloBritoMedeiros>
2024-03-17 04:01:22 +0000

    Secret Keys adicionados

--- mounting_data_access.py
+++ mounting_data_access.py
@@ -5,9 +5,9 @@ dbutils.fs.ls("/mnt")
 
 configs = {"fs.azure.account.auth.type": "OAuth",
           "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
-          "fs.azure.account.oauth2.client.id": "d01f51ef-13f7-4ffc-92cb-b34f0c418a96",
-          "fs.azure.account.oauth2.client.secret": "q9D8Q~GhDQarrP-2iZL_~qDvnSQ842NTad9.tbnJ",
-          "fs.azure.account.oauth2.client.endpoint": "https://login.microsoftonline.com/342cb166-1398-4323-91ac-d401e02b10cf/oauth2/token"}
+          "fs.azure.account.oauth2.client.id": dbutils.secrets.get(scope="myblob", key="aplication_id"),
+          "fs.azure.account.oauth2.client.secret": dbutils.secrets.get(scope="myblob", key="service-credential-key-name"),
+          "fs.azure.account.oauth2.client.endpoint": f"https://login.microsoftonline.com/{dbutils.secrets.get(scope='myblob', key='directory-id')}/oauth2/token"}
 
 # Optionally, you can add <directory-name> to the source URI of your mount point.
 dbutils.fs.mount(
